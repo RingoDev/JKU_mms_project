80876fdbdfd6525a408332752653e5d14b95df63
RingoDev <ringodev@protonmail.com>
2020-05-20 18:47:06 +0200

    added Icon because why not?

--- src/JKU_MMS/Main.java
+++ src/JKU_MMS/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -33,6 +34,7 @@ public class Main extends Application {
         Controller controller = loader.getController();
 
         primaryStage.setTitle("Video Encoder");
+        primaryStage.getIcons().add(new Image("file:icon.png"));
         mainScene = new Scene(root, 800, 400);
         primaryStage.setScene(mainScene);
         primaryStage.show();
